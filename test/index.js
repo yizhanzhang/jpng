e89acda8de1699f34550dbef3123e3dd2676d517
yizhanzhang <zhangscript@gmail.com>
2023-02-05 22:38:32 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
 const main = require('../build/Debug/main.node');
 
 console.log(main);
 const img = new main.Image();
-img.convert("yizhan.png", "yizhan_o.jpeg");
+const inputBuffer = fs.readFileSync("yizhan.png");
+console.log(inputBuffer);
+// img.convert("yizhan.png", "yizhan_o.jpeg");
