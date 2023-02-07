fa71a2eb9a06bf15b36575303ebcd17073b11f7e
yizhanzhang <yizhanzhang@tencent.com>
2023-02-07 13:59:35 +0800

    feat: update test

--- test/index.js
+++ test/index.js
@@ -2,9 +2,10 @@ const path = require('path');
 const binding = require('../binding/index');
 
 const inputPath = path.join(__dirname, "test.png");
-const outputPath = path.join(__dirname, "test_o.png");
+const outputPath1 = path.join(__dirname, "test_o.png");
+const outputPath2 = path.join(__dirname, "test_o.jpeg");
 
-console.log(binding);
 const img = new binding.Image();
 img.decode(inputPath);
-img.encode(outputPath);
+img.encode(outputPath1);
+img.encode(outputPath2);
